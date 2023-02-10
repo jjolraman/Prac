90f61274139974a10ff9a232232066a5bd9f461c
jjolraman <jjolraman@gmail.com>
2023-02-10 21:07:03 +0900

    저장용 commit

--- src/App.js
+++ src/App.js
@@ -1,61 +1,82 @@
-import { useState } from "react";
+import React from "react";
 import "./App.css";
 
 const Todo = () => {
-  const [title, setTitle] = useState("");
-  const [todo, setTodo] = useState([
-    { id: 1, title: "제목", isDone: false },
-    { id: 2, title: "제목2", isDone: false },
-  ]);
+  const [todoTitle, setTodoTitle] = React.useState("");
+  const [todos, setTodos] = React.useState([]);
 
-  const onAddTodo = () => {
-    if (title === "") return;
-    let newData = {
-      id: todo.length + 1,
+  // useRef??
 
-      title: title,
-      isDone: false,
-    };
-    let copy = [...todo, newData];
-    setTodo(copy);
+  const handleAddTodo = () => {
+    if (!todoTitle) return;
+    setTodos([
+      ...todos,
+      { id: Math.random() + Date(), title: todoTitle, isDone: false },
+    ]);
+    setTodoTitle("");
+  };
+
+  const handleToggleTodo = (id) => {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          todo.isDone = !todo.isDone;
+        }
+        return todo;
+      })
+    );
+  };
+
+  const handleRemoveTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
   };
 
   return (
     <div style={{ width: "500px" }}>
-      <h1>my todo list</h1>
+      <h1>My Todo List</h1>
       <div
-        className="titlebox"
         style={{ display: "flex", borderColor: "gray", borderStyle: "solid" }}
       >
         <div>
-          <h3>제목</h3>
-          <input type text />
+          <h3>Title</h3>
+          <input
+            type="text"
+            value={todoTitle}
+            onChange={(e) => setTodoTitle(e.target.value)}
+          />
         </div>
-        <div>
-          <h3>내용</h3>
-          <input type text />
-        </div>
-        <button onclick>추가하기</button>
+        <button onClick={handleAddTodo}>추가하기</button>
       </div>
 
       <h1>Working...!</h1>
-      <div style={{ borderColor: "blue", borderStyle: "solid" }}>
-        <h5>리액트 공부!</h5>
-        <h7>리액트 기초공부!</h7>
-        <button onclick>완료하기</button>
-        <button onclick>삭제하기</button>
-      </div>
+      {todos
+        .filter((todo) => !todo.isDone)
+        .map((todo) => (
+          <div
+            key={todo.id}
+            style={{ borderColor: "blue", borderStyle: "solid" }}
+          >
+            <h5>{todo.title}</h5>
+            <button onClick={() => handleToggleTodo(todo.id)}>완료</button>
+            <button onClick={() => handleRemoveTodo(todo.id)}>삭제하기</button>
+          </div>
+        ))}
 
       <h1>Done...!</h1>
-      <div style={{ borderColor: "red", borderStyle: "solid" }}>
-        <div>
-          <h5>리액트 공부!!@</h5>
-          <h7>리액트 기초공부!!@</h7>
-          <button onclick>완료하기</button>
-          <button onclick>삭제하기</button>
-        </div>
-      </div>
+      {todos
+        .filter((todo) => todo.isDone)
+        .map((todo) => (
+          <div
+            key={todo.id}
+            style={{ borderColor: "red", borderStyle: "solid" }}
+          >
+            <h5>{todo.title}</h5>
+            <button onClick={() => handleToggleTodo(todo.id)}>취소하기</button>
+            <button onClick={() => handleRemoveTodo(todo.id)}>삭제하기</button>
+          </div>
+        ))}
     </div>
   );
 };
+
 export default Todo;
